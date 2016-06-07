453b49e03e35610268e59f095e3d4753bc3776d4
luxidea <contact@luxidea.net>
2016-06-07 13:46:43 +0900

    画像のピクセル色を補間するマップクラスを追加。

--- _LIBRARY/LUXOPHIA/LUX.Color/LUX.Color.pas
+++ _LIBRARY/LUXOPHIA/LUX.Color/LUX.Color.pas
@@ -3,7 +3,6 @@ unit LUX.Color;
 interface //#################################################################### ■
 
 uses System.UITypes,
-     FMX.Graphics,
      LUX;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
@@ -79,6 +78,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        class operator Positive( const V_:TSingleRGB ) :TSingleRGB;
        class operator Add( const A_,B_:TSingleRGB ) :TSingleRGB;
        class operator Subtract( const A_,B_:TSingleRGB ) :TSingleRGB;
+       class operator Multiply( const A_,B_:TSingleRGB ) :TSingleRGB;
        class operator Multiply( const A_:Single; const B_:TSingleRGB ): TSingleRGB;
        class operator Multiply( const A_:TSingleRGB; const B_:Single ): TSingleRGB;
        class operator Divide( const A_:TSingleRGB; const B_:Single ): TSingleRGB;
@@ -104,7 +104,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        class operator Positive( const V_:TSingleRGBA ) :TSingleRGBA;
        class operator Add( const A_,B_:TSingleRGBA ) :TSingleRGBA;
        class operator Subtract( const A_,B_:TSingleRGBA ) :TSingleRGBA;
-       class operator Multiply( const A_,B_:TSingleRGBA ): TSingleRGBA;
+       class operator Multiply( const A_,B_:TSingleRGBA ) :TSingleRGBA;
        class operator Multiply( const A_:Single; const B_:TSingleRGBA ): TSingleRGBA;
        class operator Multiply( const A_:TSingleRGBA; const B_:Single ): TSingleRGBA;
        class operator Divide( const A_:TSingleRGBA; const B_:Single ): TSingleRGBA;
@@ -113,33 +113,11 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        class operator Implicit( const C_:TSingleRGBA ) :TAlphaColor;
        class operator Implicit( const C_:TByteRGBA ) :TSingleRGBA;
        class operator Implicit( const C_:TSingleRGBA ) :TByteRGBA;
+       class operator Implicit( const C_:TSingleRGB ) :TSingleRGBA;
+       class operator Implicit( const C_:TSingleRGBA ) :TSingleRGB;
      end;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGradation1D
-
-     TGradation1D = class
-     private
-     protected
-       _Colors  :TArray<TSingleRGBA>;
-       _ColorsN :Integer;
-       ///// アクセス
-       function GetColors( const I_:Integer ) :TSingleRGBA;
-       procedure SetColors( const I_:Integer; const Colors_:TSingleRGBA );
-       procedure SetColorsN( const ColorsN_:Integer );
-     public
-       constructor Create; overload;
-       constructor Create( const FileName_:String ); overload;
-       destructor Destroy; override;
-       ///// プロパティ
-       property Colors[ const I_:Integer ] :TSingleRGBA read GetColors write SetColors;
-       property ColorsN                    :Integer     read _ColorsN  write SetColorsN;
-       ///// メソッド
-       procedure LoadFromBitmap( const Bitmap_:TBitmap );
-       procedure LoadFromFile( const FileName_:String );
-       function Interp( const X_:Single ) :TSingleRGBA;
-     end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
 
@@ -284,6 +262,16 @@ begin
      end;
 end;
 
+class operator TSingleRGB.Multiply( const A_,B_:TSingleRGB ) :TSingleRGB;
+begin
+     with Result do
+     begin
+          R := A_.R * B_.R;
+          G := A_.G * B_.G;
+          B := A_.B * B_.B;
+     end;
+end;
+
 class operator TSingleRGB.Multiply( const A_:Single; const B_:TSingleRGB ): TSingleRGB;
 begin
      with Result do
@@ -396,7 +384,7 @@ begin
      end;
 end;
 
-class operator TSingleRGBA.Multiply( const A_,B_:TSingleRGBA ): TSingleRGBA;
+class operator TSingleRGBA.Multiply( const A_,B_:TSingleRGBA ) :TSingleRGBA;
 begin
      with Result do
      begin
@@ -474,109 +462,28 @@ begin
      end;
 end;
 
+class operator TSingleRGBA.Implicit( const C_:TSingleRGB ) :TSingleRGBA;
+begin
+     with Result do
+     begin
+          R := C_.R;
+          G := C_.G;
+          B := C_.B;
+          A := 1;
+     end;
+end;
+
+class operator TSingleRGBA.Implicit( const C_:TSingleRGBA ) :TSingleRGB;
+begin
+     with Result do
+     begin
+          R := C_.R;
+          G := C_.G;
+          B := C_.B;
+     end;
+end;
+
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGradation1D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TGradation1D.GetColors( const I_:Integer ) :TSingleRGBA;
-begin
-     Result := _Colors[ I_ ];
-end;
-
-procedure TGradation1D.SetColors( const I_:Integer; const Colors_:TSingleRGBA );
-begin
-     _Colors[ I_ ] := Colors_;
-end;
-
-procedure TGradation1D.SetColorsN( const ColorsN_:Integer );
-begin
-     _ColorsN := ColorsN_;
-
-     SetLength( _Colors, _ColorsN );
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TGradation1D.Create;
-begin
-     inherited;
-
-end;
-
-constructor TGradation1D.Create( const FileName_:String );
-begin
-     Create;
-
-     LoadFromFile( FileName_ );
-end;
-
-destructor TGradation1D.Destroy;
-begin
-
-     inherited;
-end;
-
-/////////////////////////////////////////////////////////////////////// メソッド
-
-procedure TGradation1D.LoadFromBitmap( const Bitmap_:TBitmap );
-var
-   B :TBitmapData;
-   I :Integer;
-   P :PAlphaColor;
-begin
-     Bitmap_.Map( TMapAccess.Read, B );
-
-     ColorsN := Bitmap_.Width;
-
-     P := B.GetScanline( 0 );
-     for I := 0 to _ColorsN-1 do
-     begin
-          _Colors[ I ] := P^;  Inc( P );
-     end;
-
-     Bitmap_.Unmap( B );
-end;
-
-procedure TGradation1D.LoadFromFile( const FileName_:String );
-var
-   B :TBitmap;
-begin
-     B := TBitmap.Create;
-
-     B.LoadFromFile( FileName_ );
-
-     LoadFromBitmap( B );
-
-     B.Free;
-end;
-
-function TGradation1D.Interp( const X_:Single ) :TSingleRGBA;
-var
-   Xi :Integer;
-   X, Xd :Single;
-   C0, C1 :TSingleRGBA;
-begin
-     if X_ <= 0 then Result := _Colors[ 0          ]
-     else
-     if X_ >= 1 then Result := _Colors[ _ColorsN-1 ]
-     else
-     begin
-          X := ( _ColorsN-1 ) * X_;
-
-          Xi := Floor( X );  Xd := X - Xi;
-
-          C0 := _Colors[ Xi   ];
-          C1 := _Colors[ Xi+1 ];
-
-          Result := ( C1 - C0 ) * Xd + C0;
-     end;
-end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
